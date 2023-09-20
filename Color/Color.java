5f7c4036481b5736432de9a76d9991fe2f468c67
TechMaxTro <ishabrato55@gmail.com>
2023-09-20 12:12:33 +0530

    Color

---
+++ Color/Color.java
@@ -0,0 +1,7 @@
+class Color{
+        String RESET = "\u001B[0m";
+        String RED = "\u001B[31m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        String CYAN = "\u001B[36m";
+}
