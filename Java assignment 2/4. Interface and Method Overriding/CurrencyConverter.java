a2129577d67094b58a2fee49d76671d11562fd09
TechMaxTro <ishabrato55@gmail.com>
2023-09-22 13:16:52 +0530

    added

--- Java assignment 2/4. Interface and Method Overriding/CurrencyConverter.java
+++ Java assignment 2/4. Interface and Method Overriding/CurrencyConverter.java
@@ -9,9 +9,11 @@ class EuroConverter implements Converter {
     double amt = 0;
 
     public void convertCurrency() {
-        System.out.print("\nEnter the amount to be converted: ");
+        System.out.println("\n---------------INR TO EURO CONVERTOR---------------");
+        System.out.print("Enter the amount to be converted: ");
         amt = sc.nextDouble();
         System.out.println("INR to Euro currency convertor: " + (amt * 88.50));
+        System.out.println("----------------------------------------------------");
     }
 }
 
@@ -20,33 +22,36 @@ class DollarConverter implements Converter {
     double amt = 0;
 
     public void convertCurrency() {
-        System.out.print("\nEnter the amount to be converted:");
+        System.out.println("\n---------------INR TO USD CONVERTOR---------------");
+        System.out.print("Enter the amount to be converted:");
         amt = sc.nextDouble();
         System.out.println("INR to USD currency convertor: " + (amt * 83.10));
+        System.out.println("----------------------------------------------------");
     }
 }
 
 class CurrencyConverter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        DollarConverter d = new DollarConverter();
-        EuroConverter e = new EuroConverter();
+        DollarConverter dollar = new DollarConverter();
+        EuroConverter euro = new EuroConverter();
         while (true) {
-            System.out.println("\nCurrency converters");
-            System.out.println("Choose\n (1) For INR To USD Converter\n (2) For INR To Euro Converter\n (3) For Exit");
+            System.out.println("\n Welcome Too Currency Converter");
+            System.out.println("------------Select------------\n (1) For INR To USD Converter\n (2) For INR To Euro Converter\n (3) For Exit");
             System.out.print("Enter your choice:");
             int ch = sc.nextInt();
             switch (ch) {
                 case 1:
-                    d.convertCurrency();
+                    dollar.convertCurrency();
                     break;
                 case 2:
-                    e.convertCurrency();
+                    euro.convertCurrency();
                     break;
                 case 3:
                     System.exit(0);
                 default:
                     System.out.println("Invalid Input");
+                    System.out.println("----------------------------------------------------");
             }
 
         }
